56823a753aac216b9422093eae9261d731fb72bc
Matthias Jung <majung@amazon.com>
2013-05-08 16:43:01 +0200

    gitignore and helloworld test

--- helloworld/recipes/default.rb
+++ helloworld/recipes/default.rb
@@ -1,6 +1,9 @@
 ruby_block "just an example" do
   block do
     puts "Hello"
-    puts node.inspect
+    File.open("/tmp/helloworld.log","w") do |f|
+      f.write("Hello World!")
+      f.write(node.inspect)
+    end
   end
 end
